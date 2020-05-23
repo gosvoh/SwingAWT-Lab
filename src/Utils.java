5d031fdb5eb05062b299cfc3d2d2ac74fa1ec19d
Aleksei Vokhmin <vohmina2011@yandex.ru>
2020-05-23 19:52:36 +0300

    Separated static methods to standalone Utils class

---
+++ src/Utils.java
@@ -0,0 +1,75 @@
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class Utils {
+
+    /**
+     * Добавить все переданные компоненты в указанный контейнер
+     *
+     * @param container  контейнер
+     * @param components компоненты для добавления
+     */
+    public static void addAllTo(Container container, Component... components) {
+        for (Component component : components)
+            container.add(component);
+    }
+
+    /**
+     * Установить фонт для каждого переданного элемента,
+     * если этот элемент является контейнером,
+     * то устанавливаем фонт для всех вложенных
+     * элементов
+     *
+     * @param font       фонт
+     * @param components компонент
+     */
+    public static void setFontForEach(Font font, Component... components) {
+        for (Component component : components) {
+            component.setFont(font);
+            if (component instanceof Container)
+                for (Component child : ((Container) component).getComponents())
+                    setFontForEach(font, child);
+        }
+    }
+
+    public static void updateFontFamily(String name, Component... components) {
+        Font currentFont = components[0].getFont();
+        setFontForEach(new Font(name, currentFont.getStyle(), currentFont.getSize()), components);
+        System.out.println("Updated font to " + name);
+    }
+
+    public static void updateFontStyle(int style, Component... components) {
+        Font currentFont = components[0].getFont();
+        setFontForEach(new Font(currentFont.getName(), style, currentFont.getSize()), components);
+        System.out.println("Updated font to " + style);
+    }
+
+    public static void updateFontSize(int size, Component... components) {
+        Font currentFont = components[0].getFont();
+        setFontForEach(new Font(currentFont.getName(), currentFont.getStyle(), size), components);
+        System.out.println("Updated font to " + size);
+    }
+
+    /**
+     * Создать фонт по полученной ссылке
+     *
+     * @param urlStr ссылка на фонт
+     * @return фонт
+     */
+    public static Font createFont(String urlStr) {
+        Font font = null;
+        try {
+            URL url = new URL(urlStr);
+            InputStream is = url.openStream();
+            font = Font.createFont(Font.TRUETYPE_FONT, is);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(font);
+            font = font.deriveFont(Font.BOLD, 14);
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
+        return font;
+    }
+}
